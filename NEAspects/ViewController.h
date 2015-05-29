3ce2c9e6350a9f1df7e841ee265c4bef10b7769b
yuxiaohua <hzyuxiaohua@corp.netease.com>
2015-05-29 22:09:54 +0800

    Initial Commit

---
+++ NEAspects/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NEAspects
+//
+//  Created by H-YXH on 5/29/15.
+//  Copyright (c) 2015 NetEase (hangzhou) Network Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NEAspectsTests/NEAspectsTests.m
@@ -0,0 +1,40 @@
+//
+//  NEAspectsTests.m
+//  NEAspectsTests
+//
+//  Created by H-YXH on 5/29/15.
+//  Copyright (c) 2015 NetEase (hangzhou) Network Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface NEAspectsTests : XCTestCase
+
+@end
+
+@implementation NEAspectsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
